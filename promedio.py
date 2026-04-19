2554644cd4e8cfe4b5ece4ffd0aa7e27cd4b2ac0
jorgesoriaortiz4-art <jorgesoriaortiz4@gmail.com>
2026-04-20 00:20:55 +0200

    Create promedio.py

---
+++ promedio.py
@@ -0,0 +1,11 @@
+# Jorge Soria Ortiz
+cal1 = float(input("Calificación 1: "))
+cal2 = float(input("Calificación 2: "))
+cal3 = float(input("Calificación 3: "))
+
+promedio = (cal1 + cal2 + cal3) / 3
+
+if promedio >= 6:
+    print("Aprobado:", promedio)
+else:
+    print("Reprobado:", promedio)
